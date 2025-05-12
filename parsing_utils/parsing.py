e5fec7c58228cbc0c473f432b1e41ecf4434f355
youness anflous <anayouness51@gmail.com>
2025-05-12 11:44:12 +0100

    parsing file

--- parsing_utils/parsing.py
+++ parsing_utils/parsing.py
@@ -1,9 +1,17 @@
+from	parsing_utils.rgb_msg import s_print
+from	parsing_utils.rgb_msg import w_print
+import	sys
 import	os
 
-def	check_is_root():
-	id = os.geteuid()
-	if (id != 0):
-		print("Your must be a root.")
-		exit(1)
+def	is_root():
+	if os.geteuid() != 0:
+		w_print("You must be root!!!", newline=True)
+		sys.exit(1)
 
-# check if the interface if there
+def	handle_sigint(signum, frame):
+	s_print("\nQuit\n", newline=False)
+	sys.exit(0)
+
+def clear_terminal():
+    # Clear the terminal screen
+    os.system("cls" if os.name == "nt" else "clear")
